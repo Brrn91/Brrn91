a28d5c3539cea3c06025d8a00b0682a3a861db2a
Lucas Bruno <lucasbruno81@gmail.com>
2025-11-09 15:41:19 -0300

    update ===

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const body = document.body;
 
 // Função para alternar entre tema claro e escuro
 function temaEscuro(tipo) {
-  if (tipo == true) {
+  if (tipo === true) {
     body.classList.add('escuro');
     botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
   } else {
